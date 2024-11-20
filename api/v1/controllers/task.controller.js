36bb8dc115d2aa28dd33ec974d8e44f67bb808d6
chaotic <mnrealest161@gmail.com>
2024-11-20 11:54:15 +0700

    filter status task

--- api/v1/controllers/task.controller.js
+++ api/v1/controllers/task.controller.js
@@ -2,9 +2,18 @@ const Task = require("../models/task.model");
 
 // [GET] /api/v1/tasks/
 module.exports.index = async (req, res) => {
-    const tasks = await Task.find({
+    console.log(req.query);
+    const find = {
         deleted: false
-    })
+    }
+
+    // Filter status
+    if(req.query.status) {
+        find.status = req.query.status
+    }
+    // End Filter status
+
+    const tasks = await Task.find(find)
 
     res.json(tasks);
 }
