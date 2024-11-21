a3ebf609409247892b1d4ff24cdf9a82054e44e8
chaotic <mnrealest161@gmail.com>
2024-11-21 16:19:10 +0700

    install cors in project

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const dotenv = require("dotenv");
 const database = require("./config/database");
 var bodyParser = require('body-parser');
+const cors = require("cors");
 
 dotenv.config();
 
@@ -11,6 +12,17 @@ const routerApiVer1 = require("./api/v1/routes/index.route");
 database.connect();
 
 const port = process.env.PORT;
+
+// -------------------------
+// var corsOptions = {
+//     origin: 'http://example.com',
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
+
+// app.use(cors(corsOptions));
+// -------------------------
+
+app.use(cors());
 
 // parse application/json
 app.use(bodyParser.json())
