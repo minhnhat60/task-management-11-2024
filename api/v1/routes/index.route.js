8e4d8d0a0a4ab162ba4cdbfb17edbbf5b76cec44
chaotic <mnrealest161@gmail.com>
2024-11-21 17:09:11 +0700

    register user in project

--- api/v1/routes/index.route.js
+++ api/v1/routes/index.route.js
@@ -1,10 +1,13 @@
 const taskRoutes = require("./task.route");
 const fashionRoutes = require("./fashion.route");
+const userRoutes = require("./user.route");
 
 module.exports = (app) => {
     const version = "/api/v1"
     app.use(version+"/tasks", taskRoutes);
 
     app.use(version+"/fashion", fashionRoutes);
+
+    app.use(version+"/users", userRoutes)
 
 };
