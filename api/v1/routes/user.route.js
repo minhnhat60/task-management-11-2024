c9da1c2fd51155e172eec3d2b33d5765ca69b3bd
chaotic <mnrealest161@gmail.com>
2024-11-25 18:49:44 +0700

    api user login

--- api/v1/routes/user.route.js
+++ api/v1/routes/user.route.js
@@ -4,4 +4,7 @@ const controllers = require("../controllers/user.controller");
 
 router.post("/register", controllers.register);
 
+router.post("/login", controllers.login);
+
+
 module.exports = router;
